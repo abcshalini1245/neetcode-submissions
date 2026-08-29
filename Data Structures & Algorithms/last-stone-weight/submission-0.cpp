58ebaf2e7f0298be0cb5a7e515a637cd730c9516
Shalini Upadhyay <117083412+abcshalini1245@users.noreply.github.com>
2026-08-29 16:28:05 +0530

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(int n : stones){
+            pq.push(n);
+        }
+        while(pq.size()>1){
+        int x = pq.top();
+        pq.pop();
+        int y = pq.top();
+        pq.pop();
+        
+        if(x!=y){
+           
+           pq.push(x-y);
+           
+        }
+        }
+        if(pq.size() == 0)
+          return 0;
+        else
+          return pq.top();
+    }
+};
